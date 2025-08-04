9ea68274c99100586d4ebc019bf6427882a83967
Rohan-devv <devv112004@gmail.com>
2025-08-04 21:09:12 +0530

    done

--- Client/src/ApiUri.ts
+++ Client/src/ApiUri.ts
@@ -1,2 +1,1 @@
-export const API :string= "http://localhost:8000/api/v1/users"
-// export const API :string= "https://janhit.onrender.com/api/v1/users"
+export const API: string = import.meta.env.VITE_API_URL;
